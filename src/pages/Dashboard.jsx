628dcc6e62c4e13b4347396f4432e37ae3bf1dfb
irinconc11 <maelrc11@gmail.com>
2026-04-16 12:52:40 -0500

    Conectar dashboard con backend y cards reales

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,24 @@
 import Logo from "../components/Logo"
 import MesaCard from "../components/MesaCard"
+import { useEffect, useState } from "react"
 
 function Dashboard(){
+
+    
+    const [mesas, setMesas] = useState([])
+    
+    useEffect(()=>{
+        fetch("http://localhost:8080/api/mesas", {
+            headers : {
+                "Authorization" : "Bearer " + localStorage.getItem("token")
+            }
+        })
+        .then(res=> res.json())
+        .then(data => setMesas(data))
+    },[])
+
+
+
     return(
         <div className="flex h-screen bg-gray-900">
             <div className="w-64 bg-gray-800 p-6">
@@ -22,13 +39,15 @@ function Dashboard(){
                 <div className="p-8">
                     <h1 className="text-white text-3x1 font-bold mb-8">Tus mesas</h1>
                     <div className="grid grid-cols-4 gap-6">
-                        <MesaCard numero={1} precio={8000}/>
-                        <MesaCard numero={2} precio={10000}/>
-                        <MesaCard numero={3} precio={8000}/>
-                        <MesaCard numero={4} precio={12000}/>
+                        {mesas.map(mesa => (
+                            <MesaCard
+                                key={mesa.id}
+                                numero={mesa.numero}
+                                precio={mesa.precioPorHora}
+                            />
+                        ))}
                     </div>
                 </div>
-
             </div>
         </div>
 
